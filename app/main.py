c65c19e533b14493250de2488d8022975f9d4af6
Brandeborg <Hallager2505@gmail.com>
2021-12-17 02:10:29 +0100

    finished overall structure of scheduler

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import os
-from typing import List
+from typing import Dict, List
 from fastapi import FastAPI, HTTPException, requests
 from pydantic import BaseModel
 import mysql.connector
@@ -23,9 +23,8 @@ QUOTA_SERVICE_IP = os.getenv("QUOTA_SERVICE_IP")
 MZN_SERVICE_IP = os.getenv("MZN_SERVICE_IP")
 
 
-
-# TO LIST for this file:
-    # Quning of the jobs, is that done in the service or? (if the service handels it, should there be a database connect?) - see line 139, where I think it should be implemented 
+# TO DO LIST for this file:
+    # Quning of the computations, is that done in the service or? (if the service handels it, should there be a database connect?) - see line 139, where I think it should be implemented 
     # Endpoint, that I (Thomas) don't know how works:
         # Minizin mzn-dispatcher
         # Solver list
@@ -33,32 +32,33 @@ MZN_SERVICE_IP = os.getenv("MZN_SERVICE_IP")
 
 # Add endpoint: That allow for Thomas to tell when a computation is finish
 
-# All data that needs to be added to database(s)
-class SingleComputation(BaseModel):
+# All data that needs to be added to the database
+class Computation(BaseModel):
     solver_ids: List[int]
     mzn_url: str #The URL to that point to where the minizin model file is stored. 
     dzn_url: str #The URL that points to where the minizin data fil is stored.
-    vcpus: int #The amount of Vcpu resources that this job should have
-    memory: int #The amout of memory resources that this job should have
+    vcpus: int #The amount of Vcpu resources that this computation should have
+    memory: int #The amout of memory resources that this computation should have
     solver_options: List[str]
     user_id: str # don't know the format that the guid is stored in.
 
-class ComputatitonLaunch(BaseModel):
-    computationLaunch: List[SingleComputation]
-
-class checkResources:
+class CheckResources:
     user_id: str
-    vcpu_asked: int
-    memory_asked: int
+    vcpu_requested: int
+    memory_requested: int
 
 # Request schema for when wanting to launch computation
 class LaunchComputationRequest(BaseModel):
     solver_ids: List[int]
     mzn_id: str #The id of a minizinc instance. Pointing to a database row that includes both mzn and dzn urls
-    vcpus: int #The amount of Vcpu resources that this job should have
-    memory: int #The amout of memory resources that this job should have
+    vcpus: int #The amount of Vcpu resources that this computation should have
+    memory: int #The amout of memory resources that this computation should have
     solver_options: List[str]
     user_id: str # don't know the format that the guid is stored in.
+
+class FinishComputationRequest(BaseModel):
+    user_id: str
+    computation_id: str
 
 # Expected results from endpoints used for testing:
 
@@ -70,46 +70,30 @@ app = FastAPI()
 
 @app.get("/testdb") 
 def test_db():
-    """
-    sql_write: str = "INSERT INTO scheduler (user_id, job_memory, job_vcpu, mzn_url, dzn_url) values (%s, %s, %s, %s, %s)"
-    writeDB(sql_write, ("user1", 1000, 8, "durl", "murl"))
+    computation = Computation(solver_ids = [1,2,3], 
+                    solver_options = [], 
+                    mzn_url = "url", 
+                    dzn_url = "url", 
+                    user_id = "user1", 
+                    memory=2000, 
+                    vcpus = 2)
 
-    sql_read: str = "SELECT * FROM scheduler"
-    read_after_write = readDB(sql_read)
-    print(read_after_write)
-
-    sql_delete: str = "DELETE FROM scheduler WHERE user_id = 'user1'"
-    writeDB(sql_delete)
-
-    sql_read: str = "SELECT * FROM scheduler"
-    read_after_delete = readDB(sql_read)
-    print(read_after_delete)"""
-
-    job = SingleComputation(solver_ids = [1,2,3], 
-                            solver_options = [], 
-                            mzn_url = "url", 
-                            dzn_url = "url", 
-                            user_id = "user1", 
-                            memory=2000, 
-                            vcpus = 2)
-
-    # schedule_job(job)
-    # print(get_all_user_scheduled_jobs(job.user_id))
-    # delete_scheduled_job("24")
-    launch_scheduled_job("user1")
+    # schedule_computation(computation)
+    # print(get_all_user_scheduled_computations(computation.user_id))
+    # delete_scheduled_computation("24")
+    launch_scheduled_computation("user1")
 
 
     return "wrote, read and deleted from database"
 
 
-@app.post("/LanuchSingleComputation") 
-def Launch_Single_Computation(request: LaunchComputationRequest):
-    #Checks to see if the requested job is 1 jobs with too many solvers to run in prallel. Meaning solvers > limit resources for just this job.
-    # Then the jobs should be can canceled
-    getQuotasresult = get_user_quota(request.user_id)
-    limit_vcpu = getQuotasresult.get("vCpu")
+@app.post("/scheduler/computation") 
+def create_computation(request: LaunchComputationRequest):
+    # check if the computation request is ever runnable with the user's cpu quota
+    user_quota = get_user_quota(request.user_id)
+    limit_vcpu = user_quota.get("vcpu")
     if len(request.solver_ids) > limit_vcpu:
-        raise HTTPException(status_code=403, detail="""The requested job can never be launched, 
+        raise HTTPException(status_code=403, detail="""The requested computation can never be launched, 
                                                     because the requested amount of parallel solvers (%s) 
                                                     exceeds the user's quota for vCPUs (%s)""" 
                                                     % (len(request.solver_ids), limit_vcpu))
@@ -117,8 +101,8 @@ def Launch_Single_Computation(request: LaunchComputationRequest):
     # get mzn urls from mzn id
     mzn_data = get_mzn_instance(request.mzn_id)
 
-    # create a job object with both mzn url and request data
-    job = SingleComputation(solver_ids = request.solver_ids, 
+    # create a computation object with both mzn/dzn urls and the request data
+    computation = Computation(solver_ids = request.solver_ids, 
                             solver_options = request.solver_options, 
                             mzn_url = mzn_data.mzn_url, 
                             dzn_url = mzn_data.dzn_url, 
@@ -126,40 +110,50 @@ def Launch_Single_Computation(request: LaunchComputationRequest):
                             memory = request.memory, 
                             vcpus = request.vcpus)
 
-    if(checkIfResourceIfavailable(job.user_id, job.vcpus, job.memory)):
-        launch_job(job)
+    scheduled_computations: List = get_all_user_scheduled_computations(computation.user_id)
+
+    if (len(scheduled_computations) == 0 and user_resources_are_available(computation.user_id, computation.vcpus, computation.memory)):
+        launch_computation(computation)
     else:
-        schedule_job(job)
+        schedule_computation(computation)
 
-# Calls the monitorservie and deleths the job from it.
-@app.post("/sceduler/FinishComputation")
-def finish_computation(computationID : str):
+@app.delete("/scheduler/computations/{user_id}") 
+def delete_user_computations(user_id: str):
+    scheduled_computations = get_all_user_scheduled_computations(user_id)
+    
+    for scheduled_computation in scheduled_computations:
+        scheduled_computation_id = scheduled_computation.get("scheduler_id")
+        delete_scheduled_computation(scheduled_computation_id)
 
-    answer = requests.delete(monitorCluserIP + '/monitor/process/' + computationID)
+    return "Deleted all scheduled computations associated with user"
 
-    print("The job has been deleth")
+@app.post("/scheduler/finish_computation")
+def finish_computation(request: FinishComputationRequest):
+    """Takes a message from the solver execution service, singalling an execution has terminated
+    Deletes the process from the monitor service and launches the next scheduled computation
 
-if __name__ == "__main__":
-
-    Launch_Single_Computation()
-
+    Args:
+        request (FinishComputationRequest): a computation id and a user id
+    """
+    delete_process_response = requests.delete(MONITOR_SERVICE_IP + '/monitor/process/' + request.computation_id)
+    launch_scheduled_computation(request.user_id)
 
 
 # Checks to see if the resources that a user asks for is available
-def checkIfResourceIfavailable(request: checkResources) -> bool:
+def user_resources_are_available(request: CheckResources) -> bool:
     # initialize values to allow increments later
     current_vcpu_usage = 0
     current_memory_usage = 0
     
     #Gets the limit resources for a user, by call the GetQuotasEndPoint
     getQuotaResult = get_user_quota(request.user_id)
-    limit_vcpu = getQuotaResult.get("vCpu")
+    limit_vcpu = getQuotaResult.get("vcpu")
     limit_memory = getQuotaResult.get("memory")
 
     # A list of monitored processes
     getMonitorForUserResult = get_user_monitor_processes(request.user_id)
 
-    # Checks if the current user, has any jobs running
+    # Checks if the current user, has any computations running
     if len(getMonitorForUserResult) == 0:
         return
 
@@ -172,106 +166,85 @@ def checkIfResourceIfavailable(request: checkResources) -> bool:
     available_vcpu = limit_vcpu - current_vcpu_usage
     available_memory = limit_memory - current_memory_usage
 
-    if (available_vcpu > request.vcpu_asked) and (available_memory > request.memory_asked):
+    if (available_vcpu > request.vcpu_requested) and (available_memory > request.memory_requested):
         return True
     else:
         return False
 
-def launch_scheduled_job(user_id):
-    """Find oldest scheduled job and launch it after deleting it from the "queue".
+def launch_scheduled_computation(user_id):
+    """Find oldest scheduled computation and launch it after deleting it from the "queue".
 
     Args:
         user_id (str): the id of the user
     """
-    scheduled_jobs = get_all_user_scheduled_jobs(user_id)
+    scheduled_computations = get_all_user_scheduled_computations(user_id)
 
-    if (len(scheduled_jobs) == 0):
+    if (len(scheduled_computations) == 0):
         return
 
-    oldest_scheduled_job = min(scheduled_jobs, key=lambda dict: dict["scheduler_id"])
+    oldest_scheduled_computation = min(scheduled_computations, key=lambda dict: dict["scheduler_id"])
 
-    delete_scheduled_job(oldest_scheduled_job.get("scheduler_id"))
-    launch_job(oldest_scheduled_job.get("job"))
+    delete_scheduled_computation(oldest_scheduled_computation.get("scheduler_id"))
+    launch_computation(oldest_scheduled_computation.get("computation"))
 
-def launch_job(job: SingleComputation):
-    """Contact solver execution service and launch an actual execution / job
+def launch_computation(computation: Computation):
+    """Contact solver execution service and launch an actual execution / computation
 
     Args:
-        job (SingleComputation): All the info the solver execution service needs
+        computation (Computation): All the info the solver execution service needs
 
     Returns:
         [type]: [description]
     """
-    # Start minizinc solver:
+    # Start minizinc solver: 
+    solver_execution_request = {'model_url': computation.mzn_url, 'data_url': computation.dzn_url, 'solvers': computation.solver_ids}
 
-    # The adress to the monitor service
-    url = minizinceClusterIp + '/run' 
+    solver_execution_response = requests.post(MZN_SERVICE_IP + '/run', json = solver_execution_request) 
+    solver_execution_response_body = solver_execution_response.json()
+    computation_id = solver_execution_response_body.get("computation_id")
 
-    # The request 
-    myjson = {'model_url': job.mzn_url, 'data_url': job.dzn_url, 'solvers': job.solver_ids}
+    # Post the computation to the monitor Service: 
+    # The request body
+    monitor_request = {'user_id': computation.user_id, 'computation_id': computation_id, 'vcpu_usage': computation.vcpus, 'memory_usage': computation.memory}
 
-    computation_id = requests.post(url, json = myjson) 
+    monitor_response = requests.post(MONITOR_SERVICE_IP + '/monitor/process/', json = monitor_request)
 
-    #Post the job to the monitor Service:
-
-    # The adress to the monitor service
-    url = monitorCluserIP + '/monitor/process/'  
-
-    # What to be posted to the monitor service
-    myjson = {'user_id': job.user_id, 'computation_id': computation_id, 'vcpu_usage': job.vcpus, 'memory_usage': job.memory}
-
-    # The answer has the following struct accourding to code in MonitorService:
-    
-    '''
-    class GetMonitorProcess(BaseModel):
-        id: int
-        user_id: str
-        computation_id: str
-        vcpu_usage: int
-        memory_usage: int
-    '''
-
-    # Not sure, if that is correct, or the answer should just be a status code??? 
-
-    answer = requests.post(url, json = myjson)
-
-    if answer == 200:
-        print("Job add to the monitor service")
-    
+    if monitor_response.status_code == 200:
+        print("computation add to the monitor service")
     else:
-        print("Job NOT add to monitor service")
+        print("computation NOT add to monitor service")
 
     return computation_id
     
 
-def schedule_job(job: SingleComputation):
-    """Adds job to queue
+def schedule_computation(computation: Computation):
+    """Adds computation to queue
 
     Args:
-        job (SingleComputation): The job to be scheduled
+        computation (Computation): The computation to be scheduled
     """
-    scheduler_prepared_sql: str = "INSERT INTO scheduler (user_id, job_memory, job_vcpu, mzn_url, dzn_url) values (%s, %s, %s, %s, %s)"
-    schduler_values = (job.user_id, job.memory, job.vcpus, job.mzn_url, job.dzn_url)
+    scheduler_prepared_sql: str = "INSERT INTO scheduler (user_id, memory_usage, vcpu_usage, mzn_url, dzn_url) values (%s, %s, %s, %s, %s)"
+    schduler_values = (computation.user_id, computation.memory, computation.vcpus, computation.mzn_url, computation.dzn_url)
 
     # write data to scheduler table and return the auto incremented id 
     inserted_row_scheduler_id = writeDB(scheduler_prepared_sql, schduler_values)
 
     # write all the solver_ids to the scheduler_solver table
     scheduler_solver_prepared_sql: str = "INSERT INTO scheduler_solver (scheduler_id, solver_id) values (%s, %s)"
-    for solver_id in job.solver_ids:
+    for solver_id in computation.solver_ids:
         writeDB(scheduler_solver_prepared_sql, (inserted_row_scheduler_id, solver_id))
 
-def load_scheduled_job(scheduler_id: int):
+def load_scheduled_computation(scheduler_id: int) -> Computation:
     # get all solver ids and save in a list
     scheduler_solver_prepared_sql: str = "SELECT solver_id FROM scheduler_solver WHERE scheduler_id = %s" 
     solver_id_tuples = readDB(scheduler_solver_prepared_sql, (scheduler_id,))
     solver_ids = [id_tuple[0] for id_tuple in solver_id_tuples]
 
     # get the rest of the data and save it in an object along with solver ids
-    scheduler_prepared_sql: str = "SELECT user_id, job_memory, job_vcpu, mzn_url, dzn_url FROM scheduler WHERE id = %s"
+    scheduler_prepared_sql: str = "SELECT user_id, memory_usage, vcpu_usage, mzn_url, dzn_url FROM scheduler WHERE id = %s"
     result = readDB(scheduler_prepared_sql, (scheduler_id,))[0]
 
-    scheduled_job = SingleComputation(user_id = result[0], 
+    scheduled_computation = Computation(user_id = result[0], 
                                     memory = result[1], 
                                     vcpus = result[2],
                                     mzn_url = result[3],
@@ -279,34 +252,32 @@ def load_scheduled_job(scheduler_id: int):
                                     solver_ids=solver_ids,
                                     solver_options = [])
 
-    return scheduled_job
+    return scheduled_computation
 
-def delete_scheduled_job(scheduler_id: int):
+def delete_scheduled_computation(scheduler_id: int):
     scheduler_prepared_sql: str = "DELETE FROM scheduler WHERE id = %s"
     writeDB(scheduler_prepared_sql, (scheduler_id,))
 
     scheduler_solver_prepared_sql: str = "DELETE FROM scheduler_solver WHERE scheduler_id = %s"
     writeDB(scheduler_solver_prepared_sql, (scheduler_id,))
 
-    return
-
-def get_all_user_scheduled_jobs(user_id: str):
+def get_all_user_scheduled_computations(user_id: str) -> List[Computation]:
     scheduler_prepared_sql: str = "SELECT id FROM scheduler WHERE user_id = %s"
     scheduler_values = (user_id,)
 
     scheduler_id_tuples: List[tuple] = readDB(scheduler_prepared_sql, scheduler_values)
     scheduler_ids = [id_tuple[0] for id_tuple in scheduler_id_tuples] # map list of tuples to list of ints
 
-    scheduled_jobs = []
+    scheduled_computations = []
     for scheduler_id in scheduler_ids:
-        scheduled_jobs.append({"scheduler_id": scheduler_id, "job": load_scheduled_job(scheduler_id)})
+        scheduled_computations.append({"scheduler_id": scheduler_id, "computation": load_scheduled_computation(scheduler_id)})
 
-    return scheduled_jobs
+    return scheduled_computations
 
-def get_user_quota(user_id: str):
+def get_user_quota(user_id: str) -> Dict[int, int]:
     # GetQuota:
-    #getQuotaResult = requests.get("253.2554.546565.46545/quotas/" + request.user_id) # <-- Need to be change to the internal Cluster IP, when uploaded to Google Cloud
-    getQuotaResult = {"memory": 10, "vCpu" : 15}
+    getQuotaResult = requests.get(QUOTA_SERVICE_IP + "/quotas/" + user_id)
+    getQuotaResult = {"memory": 10, "vcpu" : 15}
 
     return getQuotaResult
 
@@ -319,8 +290,8 @@ def get_mzn_instance(mzn_id: int):
 
 def get_user_monitor_processes(user_id: str):
     # Get current used resources for a user:
-    # Need to call the monitor endpoint to see if the user has any jobs running
-    #getMonitorForUserResult = requests.get("232652.2652.484/monitor/processes/"+ request.user_id) # <-- Need to be change to the internal Cluster IP, when uploaded to Google Cloud
+    # Need to call the monitor endpoint to see if the user has any computations running
+    getMonitorForUserResult = requests.get(MONITOR_SERVICE_IP + "/monitor/processes/"+ user_id)
     getMonitorForUserResult = [
         {'id': 1, 'user_id': 1, 'computation_id': 130, 'vcpu_usage': 2, 'memory_usage': 5}, 
         {'id': 1, 'user_id': 1, 'computation_id': 131, 'vcpu_usage': 4, 'memory_usage': 3}, 
